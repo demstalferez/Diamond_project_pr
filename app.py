21f125d9b6e19c5b82daf03134cf9c8de7297861
Demstalferez <33077142+demstalferez@users.noreply.github.com>
2022-07-29 06:40:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,10 @@ with tab_plots:
     tab_plots = tabs[3] #this is the fourth tab
     with tab_plots:
     
-
+         st.markdown("""
+             <iframe width="600" height="606" src="https://datastudio.google.com/reporting/dde3b825-cdae-49cc-a5c4-a09076e53e77" frameborder="0" style="border:0" allowfullscreen></iframe>
+             """, unsafe_allow_html=True)
+        
          AgGrid(diamond_sets)
 
 
