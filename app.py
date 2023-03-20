4f281f161c91d37bc0cc9118ceb8243a23411ee5
Demetrio Esteban Alférez <33077142+demstalferez@users.noreply.github.com>
2023-03-21 06:52:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ with col2:
 
 #"""--------------------------------------------------------------------------------------- MODEL CONFIGURATION-------------------------------------------------------------------------------- """
 
-model = load_model('diamond_model')
-model_quality = load_model('diamond_model_quality')
+model = load_model('diamond_model.pkl')
+model_quality = load_model('diamond_model_quality.pkl')
 
 
 #"""--------------------------------------------------------------------------------------- SIDEBARD ATRIBUTES -------------------------------------------------------------------------------- """
